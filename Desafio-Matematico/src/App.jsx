642925ca2c1b093fc9a8cb4d2e222c93d7d0ceb5
Rubenxdf <165188410+Rubenxdf@users.noreply.github.com>
2024-10-16 22:53:58 -0300

    add logica para resolver la sumas y restas modificando function App y  SumaResta
    modificación de las funciones app y sumaResta

--- Desafio-Matematico/src/App.jsx
+++ Desafio-Matematico/src/App.jsx
@@ -1,34 +1,34 @@
+// App.js
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import './App.css'
-import React, { useState } from 'react';
+import './App.css';
 import SumaResta from './components/SumaResta';
 
 function App() {
+    const [isPlaying, setIsPlaying] = useState(false);
 
-  const [isPlaying, setIsPlaying] = useState(false);
+    const VerificarClick = () => {
+        setIsPlaying(true); 
+    };
 
-  const VerificarClick = () => {
-    setIsPlaying(true); 
-  };
-
-  return (
-    <>
-    {isPlaying ? (
-        <SumaResta />
-      ) : (
-     <div className='container'>
-      <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="public/nado.jpeg" width={250}/>
-      <Card.Body>
-        <Card.Title><h2>Desafío Matematico</h2></Card.Title>
-        <Button variant="primary" onClick={VerificarClick}>Play</Button>
-      </Card.Body>
-    </Card>
-     </div>
-      )}
-      </>
-  )
+    return (
+        <>
+            {isPlaying ? (
+                <SumaResta />
+            ) : (
+                <div className="container">
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="public/nado.jpeg" width={250} />
+                        <Card.Body>
+                            <Card.Title><h2>Desafío Matemático</h2></Card.Title>
+                            <Button variant="primary" onClick={VerificarClick}>Play</Button>
+                        </Card.Body>
+                    </Card>
+                </div>
+            )}
+        </>
+    );
 }
 
-export default App
+export default App;
