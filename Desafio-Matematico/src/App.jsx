9f8d451038f11efb5e4e398c63e99da5410eda0b
Esteban Ibañez <69483001+EstebanIbz@users.noreply.github.com>
2024-10-21 18:57:56 -0300

    Actualizacion del CSS
    añadimos unos cambios al estilo dando un margen redondo a la imagen, cambiando tambien el fondo y agregando un margin para crear la separacion entre los botones

--- Desafio-Matematico/src/App.jsx
+++ Desafio-Matematico/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
             ) : (
                 <div className="container">
                     <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="public/nado.jpeg" width={250} />
+                        <Card.Img className="Imagen" variant="top" src="public/nado.jpeg" width={250} />
                         <Card.Body>
                             <Card.Title><h2>Desafío Matemático</h2></Card.Title>
                             <Button variant="primary" onClick={VerificarClick}>Play</Button>
